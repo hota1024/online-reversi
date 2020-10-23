46bffd5f4601c4cc7e058474ea0c563d503eb0a4
hota1024 <hotalog@hota1024.com>
2020-10-23 15:52:20 +0900

    🏷️ Add top page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,31 @@
+import Link from 'next/link'
+import { useContext } from 'react'
+import { AppContext } from '../contexts/AppContext'
+
 /**
  * Home component.
  */
 export const Home = (): React.ReactNode => {
+  const app = useContext(AppContext)
+
+  if (!app.io) {
+    return <>connecting...</>
+  }
+
   return (
     <>
-      <h1>test</h1>
+      <ul>
+        <li>
+          <Link href="/new">
+            <a href="/new">Create room</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/join">
+            <a href="/join">Join private room</a>
+          </Link>
+        </li>
+      </ul>
     </>
   )
 }
